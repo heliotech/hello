9be2e14a8bdb891d6a1bba1756edb235a849be0d
Sebastian Kazimierski <pykhaz@o2.pl>
2024-06-06 10:15:39 +0200

    Pierwszy commit: nowy plik hello.py

---
+++ hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# hello.py
+
+"""
+Learning git with nvim
+
+author: Sebastian Kazimierski, mgr. inż.
+
+date@time: 2024.06.06@10:12:51
+version: 0.0.1
+"""
+
+ftitle = __file__.split("/", maxsplit=-1)[-1].split(".", maxsplit=-1)[0]
+
+print(f"{ftitle}: learning git, hello world!")
